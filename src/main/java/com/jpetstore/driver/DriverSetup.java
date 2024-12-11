59a17f121eb1036953ff2cd313ac59eacebeadd9
ShibelAl <shibelalshech300@gmail.com>
2024-12-11 21:27:26 +0200

    added DriverSetup interface for all the infra classes that handles driver setup

---
+++ src/main/java/com/jpetstore/driver/DriverSetup.java
@@ -0,0 +1,11 @@
+package com.jpetstore.driver;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import java.io.IOException;
+
+public interface DriverSetup {
+    Capabilities getBrowserCapabilities();
+    Object getBrowserOptions();
+    WebDriver getWebDriver() throws IOException;
+}
