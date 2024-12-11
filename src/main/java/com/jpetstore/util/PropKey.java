b3fea69983b4de64fdac48f85d0fe7d230711736
ShibelAl <shibelalshech300@gmail.com>
2024-12-11 16:53:48 +0200

    added browser and driver related properties to the enum

--- src/main/java/com/jpetstore/util/PropKey.java
+++ src/main/java/com/jpetstore/util/PropKey.java
@@ -3,9 +3,15 @@ package com.jpetstore.util;
 public enum PropKey {
 
     URL("url"),
-    PORT("port");
+    PORT("port"),
+    BROWSER("browser"),
+    BROWSER_MANAGER("browserManager"),
 
-    private String propVal;
+    //Driver Paths
+    CHROME_DRIVER_PATH("chromeDriverPath"),
+    GECKO_DRIVER_PATH("geckoDriverPath");
+
+    private final String propVal;
 
     PropKey(String propVal) {
         this.propVal = propVal;
