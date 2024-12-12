f3cad54a1aaca43b051277cd00e15734544710b7
ShibelAl <shibelalshech300@gmail.com>
2024-12-12 17:52:55 +0200

    added method to return explicit wait with error handling

--- src/main/java/com/jpetstore/util/TimeUtil.java
+++ src/main/java/com/jpetstore/util/TimeUtil.java
@@ -11,4 +11,16 @@ public class TimeUtil {
     public static int getImplicitWait() throws IOException {
         return Integer.parseInt(PropertyReader.getInstance().getProperty(PropKey.IMPLICIT_WAIT.getPropVal()));
     }
+
+    /**
+     * Method to return implicit wait
+     * @return implicit wait
+     */
+    public static int getExplicitWait() {
+        try {
+            return Integer.parseInt(PropertyReader.getInstance().getProperty(PropKey.EXPLICIT_WAIT.getPropVal()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
