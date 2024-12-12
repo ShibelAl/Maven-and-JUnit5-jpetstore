25e6538b7b21a5eb0f4601e7b1af870143f7d3e6
ShibelAl <shibelalshech300@gmail.com>
2024-12-13 01:06:36 +0200

    added AccountsPage class

---
+++ src/main/java/com/jpetstore/pages/AccountsPage.java
@@ -0,0 +1,9 @@
+package com.jpetstore.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AccountsPage extends BasePage{
+    public AccountsPage(WebDriver driver) {
+        super(driver);
+    }
+}
