76daef93513f3176c52021983efb641801c5209e
ShibelAl <shibelalshech300@gmail.com>
2024-12-13 10:43:32 +0200

    removed try-catch as they are redundant after handling errors in the correct places

--- src/test/java/com/jpetstore/tests/MyFirstTest.java
+++ src/test/java/com/jpetstore/tests/MyFirstTest.java
@@ -2,13 +2,12 @@ package com.jpetstore.tests;
 import com.jpetstore.driver.DriverManager;
 import com.jpetstore.util.PropKey;
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
 
 
 public class MyFirstTest extends DriverManager {
 
     @Test
-    void this_is_my_first_selenium_test() throws InterruptedException, IOException {
+    void this_is_my_first_selenium_test() throws InterruptedException {
 
         String url = prop.getProperty(PropKey.URL.getPropVal());
         String port = prop.getProperty(PropKey.PORT.getPropVal());
