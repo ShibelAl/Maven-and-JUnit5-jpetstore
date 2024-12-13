0ab63755d507eabd5d55629e1c625b1cd02f23aa
ShibelAl <shibelalshech300@gmail.com>
2024-12-13 10:25:12 +0200

    added getAppUrl method to return the url of the current app in an oriented way

--- src/main/java/com/jpetstore/util/Helper.java
+++ src/main/java/com/jpetstore/util/Helper.java
@@ -1,11 +1,19 @@
 package com.jpetstore.util;
 
-import java.io.IOException;
-
 public class Helper {
 
-    public static boolean isWebDriverManager() throws IOException {
+    public static boolean isWebDriverManager() {
         return PropertyReader.getInstance().getProperty(PropKey.BROWSER_MANAGER.getPropVal()).
                 equalsIgnoreCase("webDriverManager");
+    }
+
+    /**
+     * Method to obtain app url from.
+     * @return url of the application.
+     */
+    public static String getAppUrl() {
+        String baseUrl = PropertyReader.getInstance().getProperty(PropKey.URL.getPropVal());
+        String port = PropertyReader.getInstance().getProperty(PropKey.PORT.getPropVal());
+        return baseUrl + port;
     }
 }
