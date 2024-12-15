ee44b4ddd70c6dcbffe6294f27330731fffc50ec
ShibelAl <shibelalshech300@gmail.com>
2024-12-15 22:27:39 +0200

    added three methods to add account, profile, and a method to click on a button

--- src/main/java/com/jpetstore/pages/AccountsPage.java
+++ src/main/java/com/jpetstore/pages/AccountsPage.java
@@ -1,5 +1,7 @@
 package com.jpetstore.pages;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import com.jpetstore.driver.DriverFactory;
 import org.openqa.selenium.support.PageFactory;
 
 public class AccountsPage extends BasePage {
@@ -13,7 +15,7 @@ public class AccountsPage extends BasePage {
 
     private static final String PASSWORD_INPUT = "//*[@id='Catalog']//input[@name='password']";
 
-    private static final String REAPEATED_PASSWORD_INPUT = "//*[@id='Catalog']//input[@name='repeatedPassword']";
+    private static final String REPEATED_PASSWORD_INPUT = "//*[@id='Catalog']//input[@name='repeatedPassword']";
 
     private static final String FIRST_NAME_INPUT = "//*[@id='Catalog']//input[@name='account.firstName']";
 
@@ -44,5 +46,80 @@ public class AccountsPage extends BasePage {
     private static final String ENABLE_BANNER_CHECKBOX = "//*[@id='Catalog']//input[@name='account.bannerOption']";
 
     private static final String SAVE_ACCT_INFO_BUTTON = "//*[@id='Catalog']//input[@value='Save Account Information']";
+
+
+    /**
+     *
+     * @param userName: the username of the user.
+     * @param password: the password of the user.
+     * @param repeatPassword: the password of the user, in the repeat password field.
+     * @return AccountsPage instance.
+     */
+    public AccountsPage addNewUserInformation(String userName,String password,
+                                              String repeatPassword){
+
+        enterTextIntoTextBox(By.xpath(USER_NAME_INPUT),userName);
+        enterTextIntoTextBox(By.xpath(PASSWORD_INPUT),password);
+        enterTextIntoTextBox(By.xpath(REPEATED_PASSWORD_INPUT),repeatPassword);
+
+        return this;
+    }
+
+
+    /**
+     * Method to enter account info
+     */
+    public AccountsPage addAccountInformation(String firstName,
+                                              String lastName,
+                                              String email,
+                                              String phone ,
+                                              String addr1 ,
+                                              String addr2 ,
+                                              String city ,
+                                              String state ,
+                                              String zip ,
+                                              String country){
+
+        enterTextIntoTextBox(By.xpath(FIRST_NAME_INPUT),firstName);
+        enterTextIntoTextBox(By.xpath(LAST_NAME_INPUT),lastName);
+        enterTextIntoTextBox(By.xpath(EMAIL_INPUT),email);
+        enterTextIntoTextBox(By.xpath(PHONE_INPUT),phone);
+        enterTextIntoTextBox(By.xpath(ADDR1_INPUT),addr1);
+        enterTextIntoTextBox(By.xpath(ADDR2_INPUT),addr2);
+        enterTextIntoTextBox(By.xpath(CITY_INPUT),city);
+        enterTextIntoTextBox(By.xpath(STATE_INPUT),state);
+        enterTextIntoTextBox(By.xpath(ZIP_INPUT),zip);
+        enterTextIntoTextBox(By.xpath(COUNTRY_INPUT),country);
+
+        return this;
+    }
+
+    /**
+     * Method to add profile information.
+     */
+    public AccountsPage addProfileInformation(String language,
+                                              String category,
+                                              boolean myList,
+                                              boolean myBanner){
+
+        selectByVisibleText(By.xpath(LANGUAGE_PREFERENCE_DROPDOWN),language);
+
+        selectByVisibleText(By.xpath(FAVOURITE_CATEGORY_DROPDOWN),category);
+
+        selectCheckBox(By.xpath(ENABLE_MY_LIST_CHECKBOX));
+
+        selectCheckBox(By.xpath(ENABLE_BANNER_CHECKBOX));
+
+        return this;
+    }
+
+    /**
+     * Saving account info
+     */
+    public DashBoardPage clickSaveAccountInformation(){
+
+        clickLinkOrBtn(By.xpath(SAVE_ACCT_INFO_BUTTON));
+        return new DashBoardPage(DriverFactory.driver);
+    }
 
 }
