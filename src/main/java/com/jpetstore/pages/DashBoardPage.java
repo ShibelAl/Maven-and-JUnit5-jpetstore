55f47a4fc115f9ce295b73f401682d043b27b0f2
ShibelAl <shibelalshech300@gmail.com>
2024-12-15 22:33:37 +0200

    added a function to get the greeting message after logging in to the website

--- src/main/java/com/jpetstore/pages/DashBoardPage.java
+++ src/main/java/com/jpetstore/pages/DashBoardPage.java
@@ -1,5 +1,5 @@
 package com.jpetstore.pages;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DashBoardPage extends BasePage{
@@ -7,4 +7,16 @@ public class DashBoardPage extends BasePage{
     public DashBoardPage(WebDriver driver) {
         super(driver);
     }
+
+    private static final String GREETING_MSG = "//*[@id='WelcomeContent']";
+
+    /**
+     * Get greeting message.
+     * @return the greeting message after logging in to the website.
+     */
+    public String getGreetingMessage(){
+        return getTextFromElement(By.xpath(GREETING_MSG));
+    }
+
+
 }
