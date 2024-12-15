351e6a239a5d990ac8073007bbb6fb9465e8be6a
ShibelAl <shibelalshech300@gmail.com>
2024-12-15 22:35:56 +0200

    added functions to navigate to the different pages in the website, with a Step decorator on each function to document each step when navigating

--- src/main/java/com/jpetstore/steps/JPetStoreSteps.java
+++ src/main/java/com/jpetstore/steps/JPetStoreSteps.java
@@ -1,12 +1,49 @@
 package com.jpetstore.steps;
-
 import com.jpetstore.driver.DriverManager;
+import com.jpetstore.pages.AccountsPage;
 import com.jpetstore.pages.BasePage;
+import com.jpetstore.pages.DashBoardPage;
 import com.jpetstore.pages.LoginPage;
+import io.qameta.allure.Step;
 
 public class JPetStoreSteps extends DriverManager {
 
-    private BasePage basePage = new BasePage(getDriver());
-    private LoginPage loginPage = new LoginPage(getDriver());
+    private final BasePage basePage = new BasePage(getDriver());
+    private final LoginPage loginPage = new LoginPage(getDriver());
+    private final DashBoardPage dashBoardPage = new DashBoardPage(getDriver());
+    private final AccountsPage accountsPage = new AccountsPage(getDriver());
 
+    @Step("Navigating to JPetstore application")
+    public void navigateToApp() {
+        basePage.navigateToApp();
+    }
+
+
+    @Step("Navigating to Login Page")
+    public void navigateToSignOnPage(){
+        basePage.navigateToSignOnPage();
+    }
+
+
+    @Step("Logging in with username: {0} & password: {1}")
+    public void doLogin(String username, String password) {
+        loginPage.doLogin(username, password);
+    }
+
+    @Step("Get greeting message")
+    public String getGreetingMessage() {
+        return dashBoardPage.getGreetingMessage();
+    }
+
+    @Step("Return message on invalid login")
+    public String getMessageOnInvalidLogin(){
+        return loginPage.getMessageOnInvalidLogin();
+    }
+
+    @Step("Adding new user information with userName: {0} , password: {1} , repeatPassword {2}")
+    public AccountsPage addNewUserInformation(String userName, String password,
+                                              String repeatPassword){
+
+        return accountsPage.addNewUserInformation(userName,password,repeatPassword);
+    }
 }
