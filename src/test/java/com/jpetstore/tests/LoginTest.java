e049a8612c1bc05ad1333c3ab263692a35f3d0b1
ShibelAl <shibelalshech300@gmail.com>
2024-12-15 22:38:44 +0200

    added two tests, a test for a login and a nested negative class tests

--- src/test/java/com/jpetstore/tests/LoginTest.java
+++ src/test/java/com/jpetstore/tests/LoginTest.java
@@ -1,15 +1,43 @@
 package com.jpetstore.tests;
-
-import com.jpetstore.driver.DriverManager;
-import com.jpetstore.pages.BasePage;
+import com.jpetstore.steps.JPetStoreSteps;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class LoginTest extends DriverManager {
+
+@DisplayName("Login tests")
+public class LoginTest extends JPetStoreSteps {
 
     @Test
+    @DisplayName("As a valid user, a user must be able to log in with a valid credentials")
+    @Description("Logging into the app via sign on page")
     void doLogin() {
-        BasePage basePage = new BasePage(getDriver());
-        basePage.navigateToApp();
-        basePage.navigateToSignOnPage();
+        navigateToApp();
+        navigateToSignOnPage();
+        doLogin("shibel", "test123");
+        assertEquals("Welcome shibel!", getGreetingMessage());
+
+        //Soft Assertion
+//        assertAll(
+//                () -> assertEquals("Welcome ABC!", getGreetingMessage()),
+//                () -> assertEquals("Welcome ABC!", getGreetingMessage())
+//        );
+    }
+
+    @Nested
+    class NegativeTestsLogin{
+        @DisplayName("User must not be able to login with invalid credentials")
+        @Test
+        void invalidLogin(){
+
+            navigateToApp();
+            navigateToSignOnPage();
+            doLogin("j2ee","j233434");
+
+            assertEquals("Invalid username or password. Signon failed.",
+                    getMessageOnInvalidLogin());
+        }
     }
 }
