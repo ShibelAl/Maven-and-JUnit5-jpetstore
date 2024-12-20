82fb7208edb1a80b8b31aa4c75431fde94ecd3da
ShibelAl <shibelalshech300@gmail.com>
2024-12-20 14:49:46 +0200

    added getRemoteVal, getHubUrl and getBrowserFromSystemVariable functions, for managing running Selenium Grid, and to consider taking the properties as flags from the terminal

--- src/main/java/com/jpetstore/util/SystemPropertyHelper.java
+++ src/main/java/com/jpetstore/util/SystemPropertyHelper.java
@@ -1,11 +1,14 @@
 package com.jpetstore.util;
+import com.jpetstore.driver.BrowserType;
 
 
 public class SystemPropertyHelper {
 
 
     /**
-     * Method to get value for elementScreenShot
+     * Method to override (elementScreenShot) property using the command line.
+     * If the property (elementScreenShot) is not overridden in the command line,
+     * then the value of the property will be taken from the application.properties file.
      */
     public static String getElementScreenShotValue(){
 
@@ -39,5 +42,89 @@ public class SystemPropertyHelper {
 
         System.out.println(msg);
         return valueFromPropFile;
+    }
+
+
+    /**
+     * Method to override remote value property from the command line.
+     * If the property (remote) is not overridden in the command line,
+     * then the value of the property will be taken from the application.properties file.
+     * @return remote property - True/False
+     */
+    public static String getRemoteVal(){
+
+        String msg = "";
+        String valueFromPropFile = PropertyReader.getInstance()
+                .getProperty(PropKey.REMOTE.getPropVal());
+
+        String remoteVal = System.getProperty(PropKey.REMOTE.getPropVal());
+
+        if(!remoteVal.isEmpty()){
+            if(remoteVal.equalsIgnoreCase("TRUE") ||
+                    remoteVal.equalsIgnoreCase("FALSE")){
+
+                return remoteVal;
+            }
+            else{
+                msg = "Incorrect system property " + remoteVal + " value specified for"
+                        + PropKey.REMOTE.getPropVal() +
+                        " ...defaulting to property provided by the application.properties file " +
+                        PropKey.REMOTE.getPropVal() + " : " + valueFromPropFile;
+
+                System.out.println(msg);
+            }
+        }
+
+        msg =   "No system property specified" +
+                " ...defaulting to application.properties file ->  " +
+                PropKey.REMOTE.getPropVal() + " : " + valueFromPropFile;
+
+        System.out.println(msg);
+
+        return valueFromPropFile;
+    }
+
+
+    /**
+     * Method to override hubUrl property from the command line.
+     * If the property (remote) is not overridden in the command line,
+     * then the value of the property will be taken from the application.properties file.
+     * @return hubUrl property
+     */
+    public static String getHubUrl(){
+
+        String hubUrl = System.getProperty(PropKey.HUB_URL.getPropVal());
+
+        if(!hubUrl.isEmpty()){
+            return hubUrl;
+        }
+
+        return PropertyReader.getInstance().getProperty(PropKey.HUB_URL.getPropVal());
+    }
+
+
+    /**
+     * Method to override (browser) property from the command line.
+     * This property is responsible for deciding on which browser the tests will run.
+     * If the property (browser) is not overridden in the command line,
+     * then the value of the property will be taken from the application.properties file.
+     * @return browser property
+     */
+    public static BrowserType getBrowserFromSystemVariable(){
+
+        String browser = System.getProperty(PropKey.BROWSER.getPropVal());
+        if(browser.isEmpty()){
+            browser = PropertyReader.getInstance().getProperty(PropKey.BROWSER.getPropVal());
+        }
+
+        if(browser.equalsIgnoreCase("CHROME")){
+            return BrowserType.CHROME;
+        }
+        else if (browser.equalsIgnoreCase("FIREFOX")){
+            return BrowserType.FIREFOX;
+        }
+        else {
+            return null;
+        }
     }
 }
